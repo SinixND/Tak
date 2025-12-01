756d638012765124c3b82230a2946a053a903ac9
SinixND <schaumberger_andreas@gmx.de>
2025-12-02 00:35:05 +0100

    feat(Stacks): Add Stack data structure
    - Data associated with Stacks:
        - topStoneId: Id of top stone
        - height: Height of stack including top stone
        - type: Type of the stack / top stone
    - Functions to allocate/free memory and to initialize default values

---
+++ src/components/StoneType.h
@@ -0,0 +1,15 @@
+#ifndef IG20251123234409
+#define IG20251123234409
+
+/**
+ * @brief Type and/or status of (placed) stones
+ */
+typedef enum
+{
+    NONE = 0,
+    FLAT, // Type: regular
+    WALL, // Type: regular
+    CAP,  // Type: capstone
+} StoneType;
+
+#endif
