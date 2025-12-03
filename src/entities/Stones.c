8bfba1e2f4df36f6b438f75fc0c71450417fc48e
SinixND <schaumberger_andreas@gmx.de>
2025-12-04 00:19:30 +0100

    feat(Stones): Add stones in play counter
    Add integer for stones in  play to be able to play the stone at the next unused index

--- src/entities/Stones.c
+++ src/entities/Stones.c
@@ -13,42 +13,13 @@ Stones initStones(
 {
     Stones stones = allocateStoneComponents( playerCount * stoneCountPerPlayer );
 
-    int playerId = 0;
-    int idx = 0;
+    int const maxIdx = ( playerCount * stoneCountPerPlayer ) + 1;
 
-    stones.affiliations[idx] = playerId;
-    stones.captives[idx] = 0;
-
-    for ( int n = 0; n < playerCount; ++n )
+    for ( int idx = 0; idx < maxIdx; ++idx )
     {
-        ++playerId;
-
-        for ( int m = 0; m < stoneCountPerPlayer; ++m )
-        {
-            ++idx;
-
-            stones.affiliations[idx] = playerId;
-            stones.captives[idx] = 0;
-        }
+        stones.affiliations[idx] = 0;
+        stones.captives[idx] = 0;
     }
-
-    assert(
-        stones.affiliations[0] == 0
-        && stones.captives[0] == 0
-        && "StoneId 0 is unassigned"
-    );
-
-    assert(
-        stones.affiliations[1] == 1
-        && stones.affiliations[stoneCountPerPlayer] == 1
-        && "Stone [1] to [n]: Affiliation is (player) 1"
-    );
-
-    assert(
-        stones.affiliations[stoneCountPerPlayer + 1] == 2
-        && stones.affiliations[2 * stoneCountPerPlayer] == 2
-        && "Stone [n+1] to [2*n]: Affiliation is (player) 2"
-    );
 
     return stones;
 }
@@ -63,7 +34,7 @@ Stones allocateStoneComponents( int const stoneCount )
 {
     int const maxIdx = stoneCount + 1;
 
-    Stones stones;
+    Stones stones = { .inPlay = 0 };
 
     stones.affiliations = malloc( maxIdx * sizeof( int ) );
     assert( stones.affiliations && "Bad malloc" );
