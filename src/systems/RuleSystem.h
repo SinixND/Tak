b5d86808fb4e4202e586fd4004f520803435ab7f
SinixND <schaumberger_andreas@gmx.de>
2025-12-03 17:50:12 +0100

    refactor: Separate and group data
    Reorganize `RulesData` into game constants (compiletime) and
    match constants (runtime). Also removed systems.

--- src/systems/RuleSystem.h
+++
@@ -1,9 +0,0 @@
-#ifndef IG20251124132139
-#define IG20251124132139
-
-/// Return initial regualar stone count for one player
-int getPlayerInitialRegularStoneCount( int boardSize );
-/// Return initial capstone count for one player
-int getPlayerInitialCapstoneCount( int boardSize );
-
-#endif
