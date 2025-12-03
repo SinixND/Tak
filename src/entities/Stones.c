081199b33dc0395399e0c3f8fd80d50801817762
SinixND <schaumberger_andreas@gmx.de>
2025-12-03 17:11:38 +0100

    refactor: Move allocation to init functions
    Combine functions allocation and initialization (of default
    values) into the initialization functionis. Allocation is now wrapped like
    free() is wrapped in deinit().

--- src/entities/Stones.c
+++ src/entities/Stones.c
@@ -3,6 +3,62 @@
 #include <assert.h>
 #include <stdlib.h>
 
+/// Allocate memory; Add one extra to enable invalid ID = 0
+Stones allocateStoneComponents( int const stoneCount );
+
+Stones initStones(
+    int const playerCount,
+    int const stoneCountPerPlayer
+)
+{
+    Stones stones = allocateStoneComponents( playerCount * stoneCountPerPlayer );
+
+    int playerId = 0;
+    int idx = 0;
+
+    stones.affiliations[idx] = playerId;
+    stones.captives[idx] = 0;
+
+    for ( int n = 0; n < playerCount; ++n )
+    {
+        ++playerId;
+
+        for ( int m = 0; m < stoneCountPerPlayer; ++m )
+        {
+            ++idx;
+
+            stones.affiliations[idx] = playerId;
+            stones.captives[idx] = 0;
+        }
+    }
+
+    assert(
+        stones.affiliations[0] == 0
+        && stones.captives[0] == 0
+        && "StoneId 0 is unassigned"
+    );
+
+    assert(
+        stones.affiliations[1] == 1
+        && stones.affiliations[stoneCountPerPlayer] == 1
+        && "Stone [1] to [n]: Affiliation is (player) 1"
+    );
+
+    assert(
+        stones.affiliations[stoneCountPerPlayer + 1] == 2
+        && stones.affiliations[2 * stoneCountPerPlayer] == 2
+        && "Stone [n+1] to [2*n]: Affiliation is (player) 2"
+    );
+
+    return stones;
+}
+
+void deinitStones( Stones* const stones )
+{
+    free( stones->affiliations );
+    free( stones->captives );
+}
+
 Stones allocateStoneComponents( int const stoneCount )
 {
     int const maxIdx = stoneCount + 1;
@@ -18,52 +74,3 @@ Stones allocateStoneComponents( int const stoneCount )
     return stones;
 }
 
-void initStones(
-    Stones* stones,
-    int const playerCount,
-    int const stoneCountPerPlayer
-)
-{
-    int playerId = 0;
-    int idx = 0;
-
-    stones->affiliations[idx] = playerId;
-    stones->captives[idx] = 0;
-
-    for ( int n = 0; n < playerCount; ++n )
-    {
-        ++playerId;
-
-        for ( int m = 0; m < stoneCountPerPlayer; ++m )
-        {
-            ++idx;
-
-            stones->affiliations[idx] = playerId;
-            stones->captives[idx] = 0;
-        }
-    }
-
-    assert(
-        stones->affiliations[0] == 0
-        && stones->captives[0] == 0
-        && "StoneId 0 is unassigned"
-    );
-
-    assert(
-        stones->affiliations[1] == 1
-        && stones->affiliations[stoneCountPerPlayer] == 1
-        && "Stone [1] to [n]: Affiliation is (player) 1"
-    );
-
-    assert(
-        stones->affiliations[stoneCountPerPlayer + 1] == 2
-        && stones->affiliations[2 * stoneCountPerPlayer] == 2
-        && "Stone [n+1] to [2*n]: Affiliation is (player) 2"
-    );
-}
-
-void deinitStones( Stones* const stones )
-{
-    free( stones->affiliations );
-    free( stones->captives );
-}
