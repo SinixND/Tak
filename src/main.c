0d367eb22d529e3fe99c99f6e948690c6e4442a6
SinixND <77335684+SinixND@users.noreply.github.com>
2025-12-05 13:59:42 +0100

    refactor: Wrap game data and functions

--- src/main.c
+++ src/main.c
@@ -1,40 +1,12 @@
-#include "Board.h"
-#include "GameConstants.h"
-#include "GameSettings.h"
-#include "MatchConstants.h"
-#include "Players.h"
-#include "Stacks.h"
-#include "Stones.h"
+#include "Game.h"
 
 int main( void )
 {
-    //* Adjust settings
-    GameSettings gameSettings = getDefaultSettings();
+    Game game = initGame( 0 );
 
-    //* Apply settings
-    MatchConstants matchConstants = defineMatchConstants( gameSettings.boardSize );
+    // run( &game );
 
-    //* Initialize values
-    Players players = initPlayers(
-        PLAYER_COUNT,
-        matchConstants.regularStoneReserves,
-        matchConstants.capstoneReserves
-    );
-
-    Stones stones = initStones(
-        PLAYER_COUNT,
-        getTotalReserves( matchConstants )
-    );
-
-    Stacks stacks = initStacks( gameSettings.boardSize );
-
-    Board board = initBoard( gameSettings.boardSize );
-
-    //* Deinitialize data
-    deinitPlayers( &players );
-    deinitStones( &stones );
-    deinitStacks( &stacks );
-    deinitBoard( &board );
+    deinitGame( &game );
 
     return 0;
 }
