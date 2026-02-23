fc8d98811ea0a5bd86779cee83f8ae1dd60635c1
SinixND <77335684+SinixND@users.noreply.github.com>
2026-02-23 18:36:04 +0100

    test(PlayersSystem): Updated

--- test/tests/TestPlayersSystem.h
+++ test/tests/TestPlayersSystem.h
@@ -4,29 +4,73 @@
 #include "Players.h"
 #include <unity.h>
 
-void testInitPlayers( void )
+void testNewPlayers( void )
+{
+    //* EXECUTE
+    Players players = newPlayers( 3 );
+
+    //* VERIFY
+    TEST_ASSERT_EQUAL_INT( 0, players.stonesInPlay[0] );
+    TEST_ASSERT_EQUAL_INT( 0, players.stonesInPlay[1] );
+    TEST_ASSERT_EQUAL_INT( 0, players.stonesInPlay[2] );
+    TEST_ASSERT_EQUAL_INT( 0, players.regularReserves[0] );
+    TEST_ASSERT_EQUAL_INT( 10, players.regularReserves[1] );
+    TEST_ASSERT_EQUAL_INT( 10, players.regularReserves[2] );
+    TEST_ASSERT_EQUAL_INT( 0, players.capstoneReserves[0] );
+    TEST_ASSERT_EQUAL_INT( 0, players.capstoneReserves[1] );
+    TEST_ASSERT_EQUAL_INT( 0, players.capstoneReserves[2] );
+}
+
+void testTakeFromReserves( void )
 {
     //* SETUP
-    int const playerCount = 2;
-    int const regularReserves = 1;
-    int const capstoneReserves = 1;
+    Players players = newPlayers( 5 );
 
-    //* EXECUTE
-    Players players = initPlayers(
-        playerCount,
-        regularReserves,
-        capstoneReserves
+    //* Execute
+    players = takeFromReserves(
+        players,
+        1,
+        FLAT
     );
 
     //* VERIFY
-    TEST_ASSERT_EQUAL_INT( playerCount, players.count );
-    TEST_ASSERT_EQUAL_INT( regularReserves, players.capstoneReserves[0] );
-    TEST_ASSERT_EQUAL_INT( regularReserves, players.capstoneReserves[1] );
-    TEST_ASSERT_EQUAL_INT( capstoneReserves, players.capstoneReserves[0] );
-    TEST_ASSERT_EQUAL_INT( capstoneReserves, players.capstoneReserves[1] );
+    TEST_ASSERT_EQUAL_INT(
+        0,
+        players.regularReserves[0]
+    );
 
-    //* CLEANUP
-    deinitPlayers( &players );
+    TEST_ASSERT_EQUAL_INT(
+        20,
+        players.regularReserves[1]
+    );
+
+    TEST_ASSERT_EQUAL_INT(
+        21,
+        players.regularReserves[2]
+    );
+
+    //* Execute
+    players = takeFromReserves(
+        players,
+        2,
+        CAP
+    );
+
+    //* VERIFY
+    TEST_ASSERT_EQUAL_INT(
+        0,
+        players.capstoneReserves[0]
+    );
+
+    TEST_ASSERT_EQUAL_INT(
+        1,
+        players.capstoneReserves[1]
+    );
+
+    TEST_ASSERT_EQUAL_INT(
+        0,
+        players.capstoneReserves[2]
+    );
 }
 
 #endif
