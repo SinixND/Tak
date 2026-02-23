33e364bb8682f0a5e27d73b3b24c141d62e735f5
SinixND <77335684+SinixND@users.noreply.github.com>
2026-02-23 18:56:10 +0100

    test(StacksSystem): Updated

--- test/tests/TestStacksSystem.h
+++ test/tests/TestStacksSystem.h
@@ -7,21 +7,60 @@
 
 void testInitStacks( void )
 {
-    //* SETUP
-    int const boardWidth = 1;
-
     //* EXECUTE
-    Stacks stacks = initStacks( boardWidth );
+    Stacks stacks = newStacks( 5 );
 
     //* VERIFY
-    TEST_ASSERT_EQUAL_INT( 0, stacks.onBoardCount );
-    TEST_ASSERT_EQUAL_INT( -1, stacks.boardIdxs[0] );
-    TEST_ASSERT_EQUAL_INT( -1, stacks.topStoneIdxs[0] );
+    TEST_ASSERT_EQUAL_INT( (int)NONE, stacks.types[0] );
+    TEST_ASSERT_EQUAL_INT( (int)NONE, stacks.types[25] );
     TEST_ASSERT_EQUAL_INT( 0, stacks.heights[0] );
-    TEST_ASSERT_EQUAL_INT( NONE, stacks.types[0] );
+    TEST_ASSERT_EQUAL_INT( 0, stacks.heights[25] );
+    TEST_ASSERT_EQUAL_INT( 0, stacks.affiliations[0][0] );
+    TEST_ASSERT_EQUAL_INT( 0, stacks.affiliations[25][43] );
+}
 
-    //* CLEANUP
-    deinitStacks( &stacks );
+void testPutStoneOnStack( void )
+{
+    //* SETUP
+    Stacks stacks = newStacks( 5 );
+
+    //* EXECUTE
+    stacks = putStoneOnStack(
+        stacks,
+        0,
+        1,
+        FLAT
+    );
+
+    //* VERIFY
+    TEST_ASSERT_EQUAL_INT( (int)FLAT, stacks.types[0] );
+    TEST_ASSERT_EQUAL_INT( 1, stacks.heights[0] );
+    TEST_ASSERT_EQUAL_INT( 1, stacks.affiliations[0][0] );
+
+    //* EXECUTE
+    stacks = putStoneOnStack(
+        stacks,
+        0,
+        2,
+        WALL
+    );
+
+    TEST_ASSERT_EQUAL_INT( (int)WALL, stacks.types[0] );
+    TEST_ASSERT_EQUAL_INT( 2, stacks.heights[0] );
+    TEST_ASSERT_EQUAL_INT( 2, stacks.affiliations[0][1] );
+
+    //* EXECUTE
+    stacks = putStoneOnStack(
+        stacks,
+        0,
+        1,
+        CAP
+    );
+
+    //* VERIFY
+    TEST_ASSERT_EQUAL_INT( (int)CAP, stacks.types[0] );
+    TEST_ASSERT_EQUAL_INT( 3, stacks.heights[0] );
+    TEST_ASSERT_EQUAL_INT( 1, stacks.affiliations[0][2] );
 }
 
 #endif
