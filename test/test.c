6f17a39723891b3983180d80236813f98b5205d8
SinixND <77335684+SinixND@users.noreply.github.com>
2026-03-02 17:44:32 +0100

    test(Stacks): Add tests for new functions

--- test/test.c
+++ test/test.c
@@ -17,12 +17,20 @@ int main( void )
 {
     UNITY_BEGIN();
 
+    //* Test BoardSystem
+    RUN_TEST( testNewBoard );
+    RUN_TEST( testAddStoneToBoard );
+    RUN_TEST( testRemoveStoneFromBoard );
+
     //* Test GameConstantsSystem
     RUN_TEST( testGetBaseRegularStoneReserves );
     RUN_TEST( testGetBaseCapstoneReserves );
 
     //* Test GameSystem
     RUN_TEST( testNewGame );
+    RUN_TEST( testPlayStone );
+    RUN_TEST( testUndoPlayStone );
+    RUN_TEST( testPickUpStack );
 
     //* Test MatchConfigsSystem
     RUN_TEST( testGetMatchConfigs );
@@ -39,16 +47,15 @@ int main( void )
     //* Test PositionSystem
     RUN_TEST( testPositionToBoardIndex );
 
-    //* Test BoardSystem
-    RUN_TEST( testNewBoard );
-    RUN_TEST( testAddStoneToBoard );
-    RUN_TEST( testRemoveStoneFromBoard );
-
     //* Test StackBufferSystem
     RUN_TEST( testNewStackBuffer );
+    RUN_TEST( testAppendToBottom );
+    RUN_TEST( testDropFromBottom );
 
     //* Test StackSystem
     RUN_TEST( testNewStack );
+    RUN_TEST( testPutOnTop );
+    RUN_TEST( testTakeFromTop );
 
     return UNITY_END();
 }
