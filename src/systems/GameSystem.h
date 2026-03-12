b43076b49b55c54310699af23b24cfdae8f13d2c
SinixND <77335684+SinixND@users.noreply.github.com>
2026-03-12 18:09:18 +0100

    docs(GameSystem): Add function descriptions

--- src/systems/GameSystem.h
+++ src/systems/GameSystem.h
@@ -8,7 +8,13 @@
 /// Provide boardWidth 0 for default value
 Game newGame( int boardWidth );
 
-/// From Player to Board; Add to History
+/** 
+ * @brief: From Player to Board; Add to history
+ *
+ * - Reduce reserves
+ * - Put stone on stack/square
+ * - Add action to history
+ */ 
 void placeStone(
     Game* const pGame,
     PlayerId const playerId,
@@ -17,13 +23,32 @@ void placeStone(
     StoneType const stoneType
 );
 
-/// From Board to Player; Undo History
+/** 
+ * @brief: From Board to Player; Undo History
+ *
+ * - Take one stone from stack/square
+ * - Increase reserves
+ * - Adjust history index
+ */ 
 void undoPlaceStone( Game* const pGame );
 
-/// From Player to Board; Redo History
+/** 
+ * @brief: From Player to Board; Redo History
+ *
+ * - Reduce reserves
+ * - Put stone on stack/square
+ * - Adjust history index
+ */ 
 void redoPlaceStone( Game* const pGame );
 
-/// From Board to Buffer; Add to History
+/** 
+ * @brief: From Board to Buffer; Add to History
+ *
+ * - Set stone type of buffer
+ * - Loop: Add stones to buffer
+ * - Remove stones from square 
+ * - Add action to history
+ */ 
 void pickUpStack(
     Game* const pGame,
     PlayerId const playerId,
@@ -31,23 +56,46 @@ void pickUpStack(
     RankId const rankY
 );
 
-/// From Buffer to Board; Undo History
+/** 
+ * @brief: From Buffer to Board; Undo History
+ *
+ * - Loop: Add stones to stack/square
+ * - Reset buffer
+ * - Adjust history index
+ */ 
 void undoPickUpStack( Game* const pGame );
 
-/// From Buffer to Board
+/** 
+ * @brief: From Buffer to Board
+ *
+ * - Add single stone to stack/square
+ * - Remove single stone from buffer
+ */ 
 void dropStone(
     Game* const pGame,
     FileId const fileX,
     RankId const rankY
 );
 
+/** 
+ * @brief: Undo last action performed by a player
+ *
+ * - Read history
+ * - Switch case and call appropriate function
+ * - Adjust history index
+ */ 
 void undo( Game* const pGame );
 
+/** 
+ * @brief: Redo next action performed by a player
+ *
+ * - Read history
+ * - Switch case and call appropriate function
+ * - Adjust history index
+ */ 
 void redo( Game* const pGame );
 
+/// Currently used to avoid unused function warnings
 void demo( Game* const pGame );
-
-//* NOTE: Dont forget to implement this (first undo drops from PlayerAction-Info) then drop all on origin?
-// void abortMove( Game* const pGame );
 
 #endif
