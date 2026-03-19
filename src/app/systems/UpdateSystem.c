0000e73c8e18801e9de54dc59df31f6792058709
SinixND <schaumberger_andreas@gmx.de>
2026-03-19 01:26:35 +0100

    feat[Input): Add input enums

--- src/app/systems/UpdateSystem.c
+++ src/app/systems/UpdateSystem.c
@@ -2,6 +2,7 @@
 
 void updateApp( App* const app )
 {
+    //* Check for app close input
     app->shoudClose = ( app->input.last == INPUT_Q ) ? true : false;
 }
 
--- src/platform/ncurses/InputSystem_NCurses.c
+++ src/platform/ncurses/InputSystem_NCurses.c
@@ -14,6 +14,99 @@ InputId pollInput( void )
             return INPUT_NONE;
         }
 
+        case '0':
+        {
+            return INPUT_0;
+        }
+
+        case '1':
+        {
+            return INPUT_0;
+        }
+
+        case '2':
+        {
+            return INPUT_0;
+        }
+
+        case '3':
+        {
+            return INPUT_0;
+        }
+
+        case '4':
+        {
+            return INPUT_0;
+        }
+
+        case '5':
+        {
+            return INPUT_0;
+        }
+
+        case '6':
+        {
+            return INPUT_0;
+        }
+
+        case '7':
+        {
+            return INPUT_0;
+        }
+
+        case '8':
+        {
+            return INPUT_0;
+        }
+
+        case 'a':
+        case 'A':
+        {
+            return INPUT_A;
+        }
+
+        case 'b':
+        case 'B':
+        {
+            return INPUT_B;
+        }
+
+        case 'c':
+        case 'C':
+        {
+            return INPUT_C;
+        }
+
+        case 'd':
+        case 'D':
+        {
+            return INPUT_D;
+        }
+
+        case 'e':
+        case 'E':
+        {
+            return INPUT_E;
+        }
+
+        case 'f':
+        case 'F':
+        {
+            return INPUT_F;
+        }
+
+        case 'g':
+        case 'G':
+        {
+            return INPUT_G;
+        }
+
+        case 'h':
+        case 'H':
+        {
+            return INPUT_H;
+        }
+
         case 'p':
         case 'P':
         {
@@ -24,6 +117,12 @@ InputId pollInput( void )
         case 'Q':
         {
             return INPUT_Q;
+        }
+
+        case 's':
+        case 'S':
+        {
+            return INPUT_S;
         }
     }
 }
