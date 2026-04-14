674f5eb668e29d14cd3cec943c27f88777a64c7f
SinixND <schaumberger_andreas@gmx.de>
2026-04-14 12:24:12 +0200

    refactor(FSM): Move to Engine

--- test/App/TestApp.h
+++ test/App/TestApp.h
@@ -3,7 +3,6 @@
 
 #include "App.h"
 #include "GameConstants.h"
-#include "InputId.h"
 #include <unity.h>
 
 void testNewApp( void )
@@ -11,8 +10,6 @@ void testNewApp( void )
     App app = newApp( BOARD_SIZE_DEFAULT );
 
     TEST_ASSERT_EQUAL_INT( BOARD_SIZE_DEFAULT, app.game.board.size );
-    TEST_ASSERT_EQUAL_INT( INPUT_NONE, app.inputBuffer.keyboard );
-    TEST_ASSERT_EQUAL_INT( false, app.shouldClose );
 
     app = newApp( 3 );
 
