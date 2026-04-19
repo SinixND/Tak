85fbf51577e758dc261c79af3607ab848e8a33b7
SinixND <schaumberger_andreas@gmx.de>
2026-04-19 22:31:52 +0200

    feat(Render): Render remaining board edges and labels

--- src/Platform/ncurses/Backend_NCurses_Render.c
+++ src/Platform/ncurses/Backend_NCurses_Render.c
@@ -1,23 +1,67 @@
 #include "BackendInterface.h"
+
 #include "Layout.h"
 
 #ifdef BACKEND_NCURSES
-
 #include <ncurses.h>
+
+void renderInfoPane( void );
+
+void renderFileLabels(
+    int const fileLabelsOffsetX,
+    int const boardSize
+);
+
+void renderRankLabels(
+    int const ruleLabelsOffsetY,
+    int const boardSize
+);
+
+void renderBoard(
+    int const fileLabelsOffsetX,
+    int const ruleLabelsOffsetY,
+    int const boardSize
+);
+
+void renderBoardEdges(
+    int const fileLabelsOffsetX,
+    int const ruleLabelsOffsetY,
+    int const boardSize
+);
 
 void renderStatic( App* const pApp )
 {
-    mvprintw(
-        0,
-        0,
-        "%s %i",
-        "BoardSize: ",
-        pApp->game.board.size
-    );
+    renderInfoPane();
 
     int const boardSize = pApp->game.board.size;
+    int const fileLabelsOffsetX = 1;
+    int const ruleLabelsOffsetY = 1;
 
-    //* Render info pane
+    renderFileLabels(
+        fileLabelsOffsetX,
+        boardSize
+    );
+
+    renderRankLabels(
+        ruleLabelsOffsetY,
+        boardSize
+    );
+
+    renderBoard(
+        fileLabelsOffsetX,
+        ruleLabelsOffsetY,
+        boardSize
+    );
+
+    renderBoardEdges(
+        fileLabelsOffsetX,
+        ruleLabelsOffsetY,
+        boardSize
+    );
+}
+
+void renderInfoPane( void )
+{
     for ( int paneIdx = 0; paneIdx < ( LAYOUT_PANE_HEIGHT ); ++paneIdx )
     {
         mvprintw(
@@ -27,10 +71,14 @@ void renderStatic( App* const pApp )
             LAYOUT_INFO_PANE[paneIdx]
         );
     }
+}
 
-    //* Render top file labels
-    int const fileLabelsOffsetX = 1;
-
+void renderFileLabels(
+    int const fileLabelsOffsetX,
+    int const boardSize
+)
+{
+    //* Top
     mvprintw(
         0,
         BOARD_OFFSET_X + fileLabelsOffsetX,
@@ -39,24 +87,54 @@ void renderStatic( App* const pApp )
         LAYOUT_LABELS_FILE
     );
 
-    //* Render left rank labels
-    int const ruleLabelsOffsetY = 1;
+    //* Bottom
+    mvprintw(
+        2 + ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ),
+        BOARD_OFFSET_X + fileLabelsOffsetX,
+        "%.*s",
+        fileLabelsOffsetX + 1 + ( boardSize * 4 ),
+        LAYOUT_LABELS_FILE
+    );
+}
 
+void renderRankLabels(
+    int const ruleLabelsOffsetY,
+    int const boardSize
+)
+{
     int const offsetIntoRankLabelsLayout
         = ( ( BOARD_SIZE_MAX - boardSize )
             * LAYOUT_BOARD_SQUARE_SIZE );
 
+    //* Left
     for ( int y = 0; y < ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ); ++y )
     {
         mvprintw(
-            y + ruleLabelsOffsetY,
+            ruleLabelsOffsetY + y,
             BOARD_OFFSET_X,
             "%s",
             LAYOUT_LABELS_RANK[offsetIntoRankLabelsLayout + y]
         );
     }
 
-    //* Render board
+    //* Right
+    for ( int y = 0; y < ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ); ++y )
+    {
+        mvprintw(
+            ruleLabelsOffsetY + y,
+            BOARD_OFFSET_X + 2 + ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ),
+            "%s",
+            LAYOUT_LABELS_RANK[offsetIntoRankLabelsLayout + y]
+        );
+    }
+}
+
+void renderBoard(
+    int const fileLabelsOffsetX,
+    int const ruleLabelsOffsetY,
+    int const boardSize
+)
+{
     int const gridOffsetX = BOARD_OFFSET_X + fileLabelsOffsetX;
     int const gridOffsetY = ruleLabelsOffsetY;
 
@@ -74,6 +152,53 @@ void renderStatic( App* const pApp )
                 );
             }
         }
+    }
+}
+
+void renderBoardEdges(
+    int const fileLabelsOffsetX,
+    int const ruleLabelsOffsetY,
+    int const boardSize
+)
+{
+    //* Render top board edge
+    for ( int x = 0; x < ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ) - 1; ++x )
+    {
+        mvaddch(
+            ruleLabelsOffsetY,
+            BOARD_OFFSET_X + fileLabelsOffsetX + 1 + x,
+            '-'
+        );
+    }
+
+    //* Render left board edge
+    for ( int y = 0; y < ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ) - 1; ++y )
+    {
+        mvaddch(
+            ruleLabelsOffsetY + 1 + y,
+            BOARD_OFFSET_X + fileLabelsOffsetX,
+            '|'
+        );
+    }
+
+    //* Render right board edge
+    for ( int y = 0; y < ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ) - 1; ++y )
+    {
+        mvaddch(
+            ruleLabelsOffsetY + 1 + y,
+            BOARD_OFFSET_X + fileLabelsOffsetX + ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ),
+            '|'
+        );
+    }
+
+    //* Render bottom board edge
+    for ( int x = 0; x < ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ) - 1; ++x )
+    {
+        mvaddch(
+            ruleLabelsOffsetY + ( boardSize * LAYOUT_BOARD_SQUARE_SIZE ),
+            BOARD_OFFSET_X + fileLabelsOffsetX + 1 + x,
+            '-'
+        );
     }
 }
 
