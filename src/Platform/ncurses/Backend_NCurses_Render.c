298a24661eb758d460b657b93ceac270953a9c23
SinixND <schaumberger_andreas@gmx.de>
2026-04-19 22:06:13 +0200

    feat(Render): Render grid

--- src/Platform/ncurses/Backend_NCurses_Render.c
+++ src/Platform/ncurses/Backend_NCurses_Render.c
@@ -34,7 +34,8 @@ void renderStatic( App* const pApp )
     mvprintw(
         0,
         BOARD_OFFSET_X + fileLabelsOffsetX,
-        "%s",
+        "%.*s", // Partly render file labels
+        fileLabelsOffsetX + 1 + ( boardSize * 4 ),
         LAYOUT_LABELS_FILE
     );
 
@@ -53,6 +54,26 @@ void renderStatic( App* const pApp )
             "%s",
             LAYOUT_LABELS_RANK[offsetIntoRankLabelsLayout + y]
         );
+    }
+
+    //* Render board
+    int const gridOffsetX = BOARD_OFFSET_X + fileLabelsOffsetX;
+    int const gridOffsetY = ruleLabelsOffsetY;
+
+    for ( int y = 0; y < boardSize; ++y )
+    {
+        for ( int x = 0; x < boardSize; ++x )
+        {
+            for ( int gridSquareIdx = 0; gridSquareIdx < ( LAYOUT_BOARD_SQUARE_SIZE + 1 ); ++gridSquareIdx )
+            {
+                mvprintw(
+                    gridOffsetY + ( y * ( LAYOUT_BOARD_SQUARE_SIZE ) ) + gridSquareIdx,
+                    gridOffsetX + ( x * ( LAYOUT_BOARD_SQUARE_SIZE ) ),
+                    "%s",
+                    LAYOUT_BOARD_SQUARE[gridSquareIdx]
+                );
+            }
+        }
     }
 }
 
