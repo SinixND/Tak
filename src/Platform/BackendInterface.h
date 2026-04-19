cfa5282695e63b23b4476c250b79aa208ecc74be
SinixND <schaumberger_andreas@gmx.de>
2026-04-19 21:12:21 +0200

    feat(Render): Add layout header

--- src/Platform/BackendInterface.h
+++ src/Platform/BackendInterface.h
@@ -4,22 +4,35 @@
 #include "App.h"
 
 //* Context
+
 /// Setup backend context
 void setupBackend( void );
+
 /// Close backend context
 void closeBackend( void );
 
 //* Timing
+
 /// Important: Calls `void updateFrame(App* const pApp)`
 void loopBackend( App* const pApp );
 
 //* Input
+
 /// Normalize user input from backend
 void pollInput( InputBuffer* const pInput );
 
 //* Render
-/// Render static application content
-void renderStatic( App* const pApp );
+
+/**
+ * @brief: Render static, non-changing content
+ *
+ * Renders:
+ * - Board w/ edges and square corners
+ * - Board headers (file/rank display)
+ * - Info pane (static content)
+ * - Separator line
+ */
+void renderStatic( App* const app );
 
 /// Render dynamic application content
 void renderDynamic( App* const pApp );
