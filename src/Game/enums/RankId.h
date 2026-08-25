64e05ed04682b24384c8feade3b06848da3d6bcb
SinixND <schaumberger_andreas@gmx.de>
2026-08-25 10:07:53 +0200

    fix(Typo): Rank char for RANK_7

--- src/Game/enums/RankId.h
+++ src/Game/enums/RankId.h
@@ -16,14 +16,14 @@ typedef enum RankId
 } RankId;
 
 static char const RANK_CHARS[] = {
-   [RANK_1] = '1',
-   [RANK_2] = '2',
-   [RANK_3] = '3',
-   [RANK_4] = '4',
-   [RANK_5] = '5',
-   [RANK_6] = '6',
-   [RANK_7] = '6',
-   [RANK_8] = '8',
+    [RANK_1] = '1',
+    [RANK_2] = '2',
+    [RANK_3] = '3',
+    [RANK_4] = '4',
+    [RANK_5] = '5',
+    [RANK_6] = '6',
+    [RANK_7] = '7',
+    [RANK_8] = '8',
 };
 
 #endif
